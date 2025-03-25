f0bbda7aa093f4ad5db1e2fc531caa999129d3f4
QQ-Wu2014 <qq127wu@gmail.com>
2025-03-25 20:30:13 +0800

    Create ex2-3.py

---
+++ ex2-3.py
@@ -0,0 +1,17 @@
+'''
+#WWWWWWW
+
+'''
+--------------------------------------
+mY ENGLISK IS BED
+
+--------------------------------------
+'''
+
+
+print ('Hello World')
+
+#MATH
+first = input("first:")
+second = input("second:")
+print(first+second)
